4e8840bfea17ced9817d689bb6890068cf3bdd63
anthony cantor <tony.cantor@gmail.com>
2012-05-19 23:49:16 -0600

    moved some functions from aug.c to util.h

--- src/aug.c
+++ src/aug.c
@@ -44,7 +44,7 @@
 #endif
 
 #include "vterm.h"
-
+#include "util.h"
 #include "screen.h"
 #include "err.h"
 #include "timer.h"
@@ -60,41 +60,6 @@ static struct {
 	char buf[BUF_SIZE]; /* IO buffer */
 	struct aug_conf conf;
 } g; 
-
-static int set_nonblocking(int fd) {
-	int opts;
-
-	opts = fcntl(fd, F_GETFL);
-	if (opts < 0) 
-		return -1;
-
-	opts = (opts | O_NONBLOCK);
-	if (fcntl(fd, F_SETFL, opts) < 0)
-		return -1;
-
-	return 0;
-}
-
-void change_winch(int how) {
-	sigset_t set;
-
-	if(sigemptyset(&set) != 0) 
-		err_exit(errno, "sigemptyset failed"); 
-
-	if(sigaddset(&set, SIGWINCH) != 0) 
-		err_exit(errno, "sigaddset failed");
-
-	if(sigprocmask(how, &set, NULL) != 0)
-		err_exit(errno, "sigprocmask failed");
-}
-
-static inline void block_winch() {
-	change_winch(SIG_BLOCK);
-}
-
-static inline void unblock_winch() {
-	change_winch(SIG_UNBLOCK);
-}
 
 /* handler for SIGWINCH */
 static void handler_winch(int signo) {
@@ -115,19 +80,6 @@ static void handler_winch(int signo) {
 
 	/* this should cause full screen damage and screen will be repainted */
 	vterm_set_size(g.vt, size.ws_row, size.ws_col);
-}
-
-static void write_n_or_exit(int fd, const void *buf, size_t n) {
-	ssize_t n_write, error;
-
-	if( (n_write = write(fd, buf, n) ) != (ssize_t) n) {
-		if(n_write > 0)
-			error = 0;
-		else
-			error = errno;
-		
-		err_exit(error, "partial write");
-	}
 }
 
 static void process_input(VTerm *vt, int master) {
--- src/util.h
+++ src/util.h
@@ -1,6 +1,60 @@
 #ifndef AUG_UTIL_H
 #define AUG_UTIL_H
 
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+
 #define AUG_ARRAY_SIZE(_arr) ( sizeof(_arr)/sizeof(_arr[0]) )
+
+static int set_nonblocking(int fd) {
+	int opts;
+
+	opts = fcntl(fd, F_GETFL);
+	if (opts < 0) 
+		return -1;
+
+	opts = (opts | O_NONBLOCK);
+	if (fcntl(fd, F_SETFL, opts) < 0)
+		return -1;
+
+	return 0;
+}
+
+
+static void change_winch(int how) {
+	sigset_t set;
+
+	if(sigemptyset(&set) != 0) 
+		err_exit(errno, "sigemptyset failed"); 
+
+	if(sigaddset(&set, SIGWINCH) != 0) 
+		err_exit(errno, "sigaddset failed");
+
+	if(sigprocmask(how, &set, NULL) != 0)
+		err_exit(errno, "sigprocmask failed");
+}
+
+static inline void block_winch() {
+	change_winch(SIG_BLOCK);
+}
+
+static inline void unblock_winch() {
+	change_winch(SIG_UNBLOCK);
+}
+
+static void write_n_or_exit(int fd, const void *buf, size_t n) {
+	ssize_t n_write, error;
+
+	if( (n_write = write(fd, buf, n) ) != (ssize_t) n) {
+		if(n_write > 0)
+			error = 0;
+		else
+			error = errno;
+		
+		err_exit(error, "partial write");
+	}
+}
+
 
 #endif
