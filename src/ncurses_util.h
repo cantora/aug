4fede67942e13f8a232a45b4995f233f27be3956
anthony cantor <tony.cantor@gmail.com>
2012-05-21 19:23:40 -0600

    forgot to add ncurses_util.h

---
+++ src/ncurses_util.h
@@ -0,0 +1,31 @@
+/* 
+ * Copyright 2012 anthony cantor
+ * This file is part of aug.
+ *
+ * aug is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * aug is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with aug.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef AUG_NCURSES_UTIL_H
+#define AUG_NCURSES_UTIL_H
+
+/* return zero if point (x,y) is not contained in the 
+ * window else return non-zero.
+ */
+static inline int win_contained(WINDOW *win, int y, int x) {
+	int maxx, maxy;
+
+	getmaxyx(win, maxy, maxx);	
+	return (x < maxx) && (y < maxy);
+}
+
+#endif /* AUG_NCURSES_UTIL_H */
