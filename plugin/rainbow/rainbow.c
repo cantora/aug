29331831cc8650a6b748ee4e9c20a0e6da15f2d8
anthony cantor <tony.cantor@gmail.com>
2012-12-28 04:09:35 -0700

    added plugin example: rainbow
    oooooh...pretty... (*.*)

---
+++ plugin/rainbow/rainbow.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <time.h>
+
+#include "aug_plugin.h"
+
+const char aug_plugin_name[] = "rainbow";
+
+void cell_update(
+	int rows, int cols, int *row, int *col, wchar_t *wch, 
+	attr_t *attr, int *color_pair, aug_action *action, void *user
+);
+
+const struct aug_api *g_api;
+struct aug_plugin *g_plugin;
+
+struct aug_plugin_cb g_callbacks = {
+	.input_char = NULL,
+	.cell_update = cell_update,
+	.cursor_move = NULL,
+	.screen_dims_change = NULL
+};
+
+void cell_update(int rows, int cols, int *row, int *col, wchar_t *wch, 
+		attr_t *attr, int *color_pair, aug_action *action, void *user) {
+	static int counter = 0;
+	(void)(rows);
+	(void)(cols);
+	(void)(row);
+	(void)(col);
+	(void)(wch);
+	(void)(action);
+	(void)(user);
+	
+	*color_pair = (rand() % 9)*9;
+	*attr = *attr | A_BOLD;
+}
+
+int aug_plugin_init(struct aug_plugin *plugin, const struct aug_api *api) {
+	g_plugin = plugin;	
+	g_api = api;
+
+	(*g_api->log)(g_plugin, "init\n");
+
+	g_callbacks.user = NULL;
+	(*g_api->callbacks)(g_plugin, &g_callbacks, NULL);
+
+	srand(time(NULL));
+	return 0;
+}
+
+void aug_plugin_free() {}
+
