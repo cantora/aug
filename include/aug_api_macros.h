3e1ac14ff45c6911a5c92293929a6106274976a1
anthony cantor <tony.cantor@gmail.com>
2013-01-24 14:16:40 -0700

    added aug_api_macros helper file

---
+++ include/aug_api_macros.h
@@ -0,0 +1,64 @@
+#ifndef AUG_AUG_API_MACROS_H
+#define AUG_AUG_API_MACROS_H
+
+#ifndef AUG_API_HANDLE
+#	error "you must define AUG_API_HANDLE to a global aug api handle to use " __FILE__
+#endif
+
+#ifndef AUG_PLUGIN_HANDLE
+#	error "you must define AUG_PLUGIN_HANDLE to a global hanlde to your plugin to use " __FILE__
+#endif
+
+#define AUG_API_CALL(_fn, ...) \
+	(*(AUG_API_HANDLE)->_fn)(__VA_ARGS__)
+
+#define aug_log(...) \
+	AUG_API_CALL(log, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_conf_val(...) \
+	AUG_API_CALL(conf_val, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_callbacks(...) \
+	AUG_API_CALL(callbacks, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_key_bind(...) \
+	AUG_API_CALL(key_bind, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_key_unbind(...) \
+	AUG_API_CALL(key_unbind, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_lock_screen() \
+	AUG_API_CALL(lock_screen, (AUG_PLUGIN_HANDLE))
+#define aug_unlock_screen() \
+	AUG_API_CALL(unlock_screen, (AUG_PLUGIN_HANDLE))
+#define aug_screen_win_alloc_top(...) \
+	AUG_API_CALL(screen_win_alloc_top, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_win_alloc_bot(...) \
+	AUG_API_CALL(screen_win_alloc_bot, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_win_alloc_left(...) \
+	AUG_API_CALL(screen_win_alloc_left, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_win_alloc_right(...) \
+	AUG_API_CALL(screen_win_alloc_right, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_win_dealloc(...) \
+	AUG_API_CALL(screen_win_dealloc, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_panel_alloc(...) \
+	AUG_API_CALL(screen_panel_alloc, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_panel_dealloc(...) \
+	AUG_API_CALL(screen_panel_dealloc, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_panel_size(...) \
+	AUG_API_CALL(screen_panel_size, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_screen_panel_update() \
+	AUG_API_CALL(screen_panel_update, (AUG_PLUGIN_HANDLE))
+#define aug_screen_doupdate() \
+	AUG_API_CALL(screen_doupdate, (AUG_PLUGIN_HANDLE) )
+#define aug_terminal_new(...) \
+	AUG_API_CALL(terminal_new, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_terminal_delete(...) \
+	AUG_API_CALL(terminal_delete, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_terminal_pid(...) \
+	AUG_API_CALL(terminal_pid, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_terminal_terminated(...) \
+	AUG_API_CALL(terminal_terminated, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_terminal_run(...) \
+	AUG_API_CALL(terminal_run, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_terminal_input(...) \
+	AUG_API_CALL(terminal_input, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+#define aug_terminal_input_chars(...) \
+	AUG_API_CALL(terminal_input_chars, (AUG_PLUGIN_HANDLE), __VA_ARGS__ )
+
+#endif /* AUG_AUG_API_H */
