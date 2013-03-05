6711f540ac6f8c0b7f3440ebcfa1cc0e28080857
anthony cantor <tony.cantor@gmail.com>
2013-03-05 15:36:19 -0700

    define macros for extended curses api

--- include/ncurses.h
+++ include/ncurses.h
@@ -1,6 +1,14 @@
 #ifndef AUG_NCURSES_H
 #define AUG_NCURSES_H
 
+#ifndef _XOPEN_SOURCE
+#	define _XOPEN_SOURCE 700
+#endif
+
+#ifndef _XOPEN_SOURCE_EXTENDED
+#	define _XOPEN_SOURCE_EXTENDED 1
+#endif
+
 #if defined(__APPLE__)
 #	include <ncurses.h>
 #else
