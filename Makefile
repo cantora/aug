cantora/aug

commit b314952d2f1850d62167afeac27ebafd632064b7

fixed some initialization bugs pointed out by valgrind
also added a valgrind target in makefile along with
a suppression file.

Makefile
@@ -50,6 +50,9 @@ default: all
 .PHONY: all
 all: $(OUTPUT) $(PLUGIN_OBJECTS)
 
+grind-aug: all
+	valgrind --log-file=./grind.log --leak-check=yes --suppressions=./.aug.supp $(CURDIR)/aug -d ./aug.log
+
 .PHONY: .FORCE
 .FORCE:
 
@@ -167,6 +170,7 @@ clean:
 	rm -f $(OUTPUT)
 	for i in $(PLUGIN_DIRS); do dir=$$i; echo "clean $$dir"; $(MAKE) $(MFLAGS) -C $$dir clean; done
 	rm -rvf ./sandbox/plugin/*
+	rm -f aug.log grind.log
 
 .PHONY: libclean
 libclean: clean

src/term.c
@@ -98,6 +98,7 @@ static int term_resize_master(const struct aug_term *term) {
 		return -1;
 
 	term_dims(term, (int *) &size.ws_row, (int *) &size.ws_col);
+	memset(&size, 0, sizeof(size)); /* to make valgrind happy */
 	if(ioctl(term->master, TIOCSWINSZ, &size) != 0)
 		return -1; 
 

.aug.supp
@@ -1,0 +1,15 @@
+{
+   forkpty_leaks_some_memory
+   Memcheck:Leak
+   fun:malloc
+   fun:nss_parse_service_list
+   fun:__nss_database_lookup
+   obj:*
+   fun:getgrnam_r@@GLIBC_2.2.5
+   fun:grantpt
+   fun:openpty
+   fun:forkpty
+   fun:child_init
+   fun:aug_main
+   fun:main
+}

.gitignore
@@ -13,3 +13,5 @@ libvterm
 script
 sandbox/plugin/*
 core
+aug.log
+grind.log

src/aug.c
@@ -773,6 +773,7 @@ int aug_cell_update(int rows, int cols, int *row, int *col,
 		if(i->plugin.callbacks == NULL || i->plugin.callbacks->cell_update == NULL)
 			continue;
 
+		action = AUG_ACT_OK;
 		(*(i->plugin.callbacks->cell_update))(
 			rows, cols, row, col, 
 			wch, attr, color_pair,
@@ -948,6 +949,7 @@ static void push_key(struct aug_term *term, uint32_t ch) {
 		if(i->plugin.callbacks == NULL || i->plugin.callbacks->input_char == NULL)
 			continue;
 		
+		action = AUG_ACT_OK;
 		(*(i->plugin.callbacks->input_char))(&ch, &action, i->plugin.callbacks->user);
 
 		if(action == AUG_ACT_CANCEL) /* plugin wants to filter this character */

src/child.c
@@ -34,6 +34,7 @@ void child_init(struct aug_child *child, struct aug_term *term,
 	struct winsize size;
 	int master;
 
+	memset(&size, 0, sizeof(size)); /* to make valgrind happy */
 	child->pid = forkpty(&master, NULL, child_termios, &size);
 	if(child->pid == 0) {
 		(*exec_cb)();

commit 2c6a59fef6b3bc724dd75e02c203da78f2437c26

fixed bug introduced in commit b314952d2f1850d62167afeac27ebafd632064b7
input winsize variable was accidentally being zero'd out

Makefile
@@ -6,7 +6,8 @@ DEFAULT_TERM	= \"screen\"
 DEFINES			= -D_XOPEN_SOURCE=700 -D_XOPEN_SOURCE_EXTENDED=1 -D_BSD_SOURCE
 DEFINES			+= -DAUG_DEFAULT_TERM=$(DEFAULT_TERM)
 DEFINES			+= -DAUG_DEFAULT_ARGV=$(DEFAULT_CMD)
-DEFINES			+= -DAUG_DEBUG -DAUG_LOCK_DEBUG
+DEFINES			+= -DAUG_DEBUG 
+#DEFINES			+= -DAUG_LOCK_DEBUG
 #DEFINES			+= -DAUG_ERR_COREDUMP
 #DEFINES			+= -DAUG_LOCK_DEBUG_PRINT
 

src/term.c
@@ -97,8 +97,8 @@ static int term_resize_master(const struct aug_term *term) {
 	if(term->master == 0)
 		return -1;
 
-	term_dims(term, (int *) &size.ws_row, (int *) &size.ws_col);
 	memset(&size, 0, sizeof(size)); /* to make valgrind happy */
+	term_dims(term, (int *) &size.ws_row, (int *) &size.ws_col);
 	if(ioctl(term->master, TIOCSWINSZ, &size) != 0)
 		return -1; 
 