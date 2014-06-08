4d04e7d9f621267f94cb5570fccfb5180d2c3801
anthony cantor <ar.cantor@gmail.com>
2014-06-07 19:32:09 -0700

    fixed bug/name duplication in aug_api.h
    i should be using the defines AUG_API_HANDLE and AUG_PLUGIN_HANDLE
    everywhere

--- include/aug_api.h
+++ include/aug_api.h
@@ -24,22 +24,21 @@
 
 #include "aug.h"
 
-extern const struct aug_api *aug_api;
-extern struct aug_plugin *aug_plugin;
-
 #define AUG_API_HANDLE aug_api_handle
 #define AUG_PLUGIN_HANDLE aug_plugin_handle
+extern const struct aug_api * AUG_API_HANDLE;
+extern struct aug_plugin * AUG_PLUGIN_HANDLE;
+
 #include "aug_api_macros.h"
 
 #define AUG_GLOBAL_API_OBJECTS \
-	const struct aug_api *aug_api_handle; \
-	struct aug_plugin *aug_plugin_handle;
-
+	const struct aug_api * AUG_API_HANDLE; \
+	struct aug_plugin * AUG_PLUGIN_HANDLE;
 
 #define AUG_API_INIT(plugin, api) \
 	do { \
-		aug_api_handle = api; \
-		aug_plugin_handle = plugin; \
+		AUG_API_HANDLE = api; \
+		AUG_PLUGIN_HANDLE = plugin; \
 	} while(0)
 
 #endif /* AUG_AUG_API_H */
